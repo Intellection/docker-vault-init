eea690ac7af75bd10e3be810f719ab7183c5825f
Hadrian Valentine <hadrian@intellectionsoftware.com>
2019-10-11 15:23:19 +0200

    Remove Viper

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
 )
 
 
@@ -126,33 +123,7 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-}
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".vault-init" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".vault-init")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 func checkError(e error) {
